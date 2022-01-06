36a5c016f074d838e41b8b582b728e47e0792fb2
Pedro Vinícius de Melo Silva <65138009+PedroViniciusMelo@users.noreply.github.com>
2022-01-06 10:07:33 -0300

    Adicionado fontawesome ao projeto

--- resources/views/components/sidebar/content.blade.php
+++ resources/views/components/sidebar/content.blade.php
@@ -1,10 +1,65 @@
 <x-perfect-scrollbar as="nav" aria-label="main" class="flex flex-col flex-1 gap-4 px-3">
-
     <x-sidebar.link title="Dashboard" href="{{ route('dashboard') }}" :isActive="request()->routeIs('dashboard')">
         <x-slot name="icon">
-            <x-icons.dashboard class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
+            <i class="fas fa-tachometer-alt"></i>
         </x-slot>
     </x-sidebar.link>
+    <x-sidebar.link title="User Profile" href="{{ route('dashboard') }}" :isActive="request()->routeIs('profile.edit')">
+        <x-slot name="icon">
+            <i class="fas fa-user-circle"></i>
+        </x-slot>
+    </x-sidebar.link>
+    @if(Auth::user()->isAdmin())
+        <x-sidebar.link title="Admin area" href="{{ route('dashboard') }}" :isActive="request()->routeIs('admin.area')">
+            <x-slot name="icon">
+                <i class="fas fa-chart-bar"></i>
+            </x-slot>
+        </x-sidebar.link>
+        <x-sidebar.link title="Solicitações" href="{{ route('dashboard') }}" :isActive="request()->routeIs('admin.area.requests')">
+            <x-slot name="icon">
+                <i class="fas fa-chart-bar"></i>
+            </x-slot>
+        </x-sidebar.link>
+        <x-sidebar.link title="Machines" href="{{ route('dashboard') }}" :isActive="request()->routeIs('user.machines')">
+            <x-slot name="icon">
+                <i class="fas fa-desktop"></i>
+            </x-slot>
+        </x-sidebar.link>
+        <x-sidebar.link title="Images" href="{{ route('dashboard') }}" :isActive="request()->routeIs('images.index')">
+            <x-slot name="icon">
+                <i class="fab fa-docker"></i>
+            </x-slot>
+        </x-sidebar.link>
+        <x-sidebar.link title="My containers" href="{{ route('dashboard') }}" :isActive="request()->routeIs('instance.index')">
+            <x-slot name="icon">
+                <i class="fas fa-server"></i>
+            </x-slot>
+        </x-sidebar.link>
+        <x-sidebar.link title="Dockerfiles" href="{{ route('dashboard') }}" :isActive="request()->routeIs('admin.area.dockerfiles')">
+            <x-slot name="icon">
+                <i class="fas fa-server"></i>
+            </x-slot>
+        </x-sidebar.link>
+    @endif
+    @if(Auth::user()->isBasic() || Auth::user()->isAdmin())
+        <x-sidebar.link title="Basic" href="{{ route('dashboard') }}" :isActive="request()->routeIs('aluno.basic.index')">
+            <x-slot name="icon">
+                <i class="fas fa-star-half-alt"></i>
+            </x-slot>
+        </x-sidebar.link>
+        <x-sidebar.link title="Containers" href="{{ route('dashboard') }}" :isActive="request()->routeIs('aluno.basic.containers')">
+            <x-slot name="icon">
+                <i class="fas fa-server"></i>
+            </x-slot>
+        </x-sidebar.link>
+    @endif
+    @if(Auth::user()->isAdvanced() || Auth::user()->isAdmin())
+        <x-sidebar.link title="Advanced" href="{{ route('dashboard') }}" :isActive="request()->routeIs('aluno.advanced.index')">
+            <x-slot name="icon">
+                <i class="fas fa-star"></i>
+            </x-slot>
+        </x-sidebar.link>
+    @endif
 
     <x-sidebar.dropdown title="Buttons" :active="Str::startsWith(request()->route()->uri(), 'buttons')">
         <x-slot name="icon">
@@ -19,14 +74,5 @@
             :active="request()->routeIs('buttons.text-icon')" />
     </x-sidebar.dropdown>
 
-    <div x-transition x-show="isSidebarOpen || isSidebarHovered" class="text-sm text-gray-500">Dummy Links</div>
 
-    @php
-        $links = array_fill(0, 20, '');
-    @endphp
-
-    @foreach ($links as $index => $link)
-        <x-sidebar.link title="Dummy link {{ $index + 1 }}" href="#" />
-    @endforeach
-       
 </x-perfect-scrollbar>
